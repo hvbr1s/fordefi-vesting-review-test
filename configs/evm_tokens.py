0afd28b6fc132d8afa2b63e28ba84ac960750c23
Dan Jaheny <danjaheny@mac.home>
2025-02-10 13:50:17 +0100

    adding nfe support

--- configs/evm_tokens.py
+++ configs/evm_tokens.py
@@ -7,18 +7,14 @@ EVM_TOKEN_CONFIGS = {
             "contract_address": "0x55d398326f99059fF775485246999027B3197955",
             "decimals": 18
         },
-        "btcb": {
-            "contract_address": "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c",
+        "nfe": {
+            "contract_address": "0xf03Ca04Dd56D695a410f46F14feF4028B22FB79A",
             "decimals": 18
         }
     },
     "ethereum": {
         "eth": {
             "decimals": 18
-        },
-        "usdt": {
-            "contract_address": "0xdAC17F958D2ee523a2206206994597C13D831ec7",
-            "decimals": 6
         },
         "pepe": {
             "contract_address": "0x6982508145454Ce325dDbE47a25d4ec3d2311933",
